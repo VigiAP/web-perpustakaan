14bd1909a86c4f7b02dd7e65df2c26d70de0c34b
VigiAP <vigi.ap@unsub.ac.id>
2024-06-15 15:18:50 +0700

    s

--- app/Views/Home/index.php
+++ app/Views/Home/index.php
@@ -5,37 +5,29 @@
         <h2>Baru Ditambahkan</h2>
         <hr>
         <?php foreach ($newBooks as $book): ?>
-        <div class="col-md-3">
-            <div class="card-sl">
-                <div class="card-image">
-                    <img src="<?= base_url('cover/' . $book['cover']); ?>" class="card-img-top"
-                        alt="<?= $book['judul'] ?>" style="height: 12rem; object-fit: cover;">
-                </div>
+            <div class="col-md-3">
+                <div class="card-sl">
+                    <div class="card-image">
+                        <img src="<?= base_url('cover/' . $book['cover']); ?>" class="card-img-top" alt="<?= $book['judul'] ?>" style="height: 12rem; object-fit: cover;">
+                    </div>
 
-                <div class="card-action">
-
-                    <a href="<?= base_url('bookmark/add/' . $book['id_buku']) ?>" class="add-to-bookmark"><i
-                            class="fa fa-bookmark"></i></a>
+                    <a class="card-action" href="#"><i class="fa fa-heart"></i></a>
+                    <div class="card-heading">
+                        <?= $book['judul'] ?>
+                    </div>
+                    <div class="card-text">
+                        <?= $book['penulis'] ?>
+                    </div>
+                    <div class="card-text">
+                        <?= $book['penerbit'] ?>
+                    </div>
+                    <div class="card-text">
+                        <?= $book['tahun_terbit'] ?>
+                    </div>
+                    <a href="#" class="card-button">Pinjam Buku</a>
                 </div>
             </div>
-            <div class="card-heading">
-                <?= $book['judul'] ?>
-            </div>
-            <div class="card-text">
-                <?= $book['penulis'] ?>
-            </div>
-            <div class="card-text">
-                <?= $book['penerbit'] ?>
-            </div>
-            <div class="card-text">
-                <?= $book['tahun_terbit'] ?>
-            </div>
-            <a href="#" class="card-button">Pinjam Buku</a>
-        </div>
+        <?php endforeach; ?>
     </div>
-    <?php endforeach; ?>
 </div>
-</div>
-
-
 <?= $this->endSection(); ?>
