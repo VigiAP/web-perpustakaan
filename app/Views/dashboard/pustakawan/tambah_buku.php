c395c82dd3eb897b39362ce78907a763524b3acc
Farma27 <farmafnr12@gmail.com>
2024-06-15 14:25:24 +0700

    add validation, change method name, add edit_anggota

--- app/Views/dashboard/pustakawan/tambah_buku.php
+++ app/Views/dashboard/pustakawan/tambah_buku.php
@@ -30,10 +30,8 @@
                 <script src="https://code.jquery.com/jquery-3.7.1.js"
                     integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
                 <select name="kategori[]" class="form-control select2" multiple="multiple" placeholder="Kategori ...">
-                    <?php
-                                foreach ($kategori as $option) : ?>
+                    <?php foreach ($kategori as $option) : ?>
                     <option value="<?= $option['id_kategori']; ?>"><?= $option['nama_kategori']; ?></option>
-
                     <?php endforeach; ?>
                 </select>
                 <script>
@@ -63,6 +61,5 @@
         </form>
     </div>
 </div>
-
 
 <?php $this->endSection(); ?>
