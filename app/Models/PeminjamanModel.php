353570424476869605b77bf91bcb41136a6410e3
Farma27 <farmafnr12@gmail.com>
2024-06-15 17:22:55 +0700

    make laporan

--- app/Models/PeminjamanModel.php
+++ app/Models/PeminjamanModel.php
@@ -20,7 +20,7 @@ class PeminjamanModel extends Model
     public function sirkulasiPeminjaman()
     {
         return $this->db->table('peminjaman a')
-            ->select('a.id_peminjaman, a.created_at, a.jatuh_tempo, a.updated_at, a.denda, GROUP_CONCAT(dp.id_buku) AS id_buku, GROUP_CONCAT(b.judul) AS judul, GROUP_CONCAT(dp.id_users) AS id_user, GROUP_CONCAT(u.username) AS peminjam')
+            ->select('a.id_peminjaman, a.created_at, a.updated_at, a.denda, GROUP_CONCAT(dp.id_buku) AS id_buku, GROUP_CONCAT(b.judul) AS judul, GROUP_CONCAT(dp.id_users) AS id_user, GROUP_CONCAT(u.username) AS peminjam')
             ->join('detail_peminjaman dp', 'a.id_peminjaman = dp.id_peminjaman')
             ->join('buku b', 'dp.id_buku = b.id_buku')
             ->join('users u', 'dp.id_users = u.id_users')
@@ -32,6 +32,14 @@ class PeminjamanModel extends Model
 
     public function laporanPeminjaman()
     {
-        
+        return $this->db->table('peminjaman a')
+            ->select('a.id_peminjaman, a.created_at, a.jatuh_tempo, a.updated_at, a.denda, GROUP_CONCAT(dp.id_buku) AS id_buku, GROUP_CONCAT(b.judul) AS judul, GROUP_CONCAT(dp.id_users) AS id_user, GROUP_CONCAT(u.username) AS peminjam')
+            ->join('detail_peminjaman dp', 'a.id_peminjaman = dp.id_peminjaman')
+            ->join('buku b', 'dp.id_buku = b.id_buku')
+            ->join('users u', 'dp.id_users = u.id_users')
+            ->groupBy('a.id_peminjaman')
+            ->OrderBy('a.created_at', 'DESC')
+            ->get()
+            ->getResult();
     }
 }
